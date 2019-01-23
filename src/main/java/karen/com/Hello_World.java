10756c55b5fdad3c21e98487048f1b1457dcfdff
karenlicoding <eslite0220@gmail.com>
2019-01-23 22:36:56 +0800

    java variable. data type

--- src/main/java/karen/com/Hello_World.java
+++ src/main/java/karen/com/Hello_World.java
@@ -6,5 +6,16 @@ public class Hello_World {
 //        new Person().hello();
           Person p = new Person();
           p.hello();
+          int age = 19;
+          Integer age2 = 19;
+          char w = '我';
+          Character w2 = '我';
+          byte b = 120;
+          float weight = 66.5f;
+          double weight2 = 66.5;
+          boolean adult = true;
+          boolean enroll = false;
+          String name = "karen";
+
     }
 }
