66b7cd2b287d82cbe657a1339c2ef58a9561944f
Karen_Home <karen_files@KarenHomedeMBP.Home>
2019-01-23 20:12:45 +0800

    Initial commit

---
+++ src/main/java/karen/com/Hello_World.java
@@ -0,0 +1,10 @@
+package karen.com;
+
+public class Hello_World {
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//        new Person().hello();
+          Person p = new Person();
+          p.hello();
+    }
+}
---
+++ src/main/kotlin/karen/com/Hello_World.kt
@@ -0,0 +1,18 @@
+package karen.com
+
+fun main(args: Array<String>) {
+//    println("Hello World")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+
+
+class Human {
+    fun hello(){
+        println("Hello Kotlin")
+    }
+
+}
+
